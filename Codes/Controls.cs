9a15ea770bde171e3b45fb350bd4de503d064346
Urgus <alfonsmitic@gmail.com>
2016-01-27 21:55:07 +0100

    New build

---
+++ Codes/Controls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+	public float PlayerSpeed = 5.0f;
+
+	//void start = first frame what to do
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//movement
+		transform.Translate (Vector3.up * Input.GetAxis ("Jump") * PlayerSpeed * Time.deltaTime);
+		transform.Translate (Vector3.forward * Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime);//0.0.1
+		}
+	}
