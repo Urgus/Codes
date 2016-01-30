3dc77a58092651e816b3ab7e7c83bb7d99af9f13
Urgus <alfonsmitic@gmail.com>
2016-01-30 10:50:14 +0100

    UploadedTheWholeProject

---
+++ UbotWholeProject/Assets/Scripts/startMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class startMenu : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+	
+	void Start () 
+	{
+	
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> (); 
+		quitMenu.enabled = false;
+	}
+
+	public void ExitPress()
+
+	{
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void noPress()
+	
+	{
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartLevel()
+
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void ExitGame()
+	
+	{
+		Application.Quit();
+	}
+
+	void Update () {
+
+	}
+
+}
