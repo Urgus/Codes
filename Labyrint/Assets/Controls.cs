2aab9573de4a1cc9ba4d793eb8af2a63ed06b70e
Urgus <alfonsmitic@gmail.com>
2016-02-23 15:59:14 +0100

    New Project!

---
+++ Labyrint/Assets/Controls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	
+	public float PlayerSpeed = 5.0f;
+	
+	//void start = first frame what to do
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//movement
+		transform.Translate (Vector2.right * Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime);//1.0.0
+		transform.Translate (Vector3.forward * Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime);//0.0.1
+	}
+}
